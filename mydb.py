fed09b40be77bc3de3541a25ef1334674c3c9614
Ali Seidu <aliseidu8855@gmail.com>
2025-03-14 14:05:24 +0000

    Update mydb.py

--- mydb.py
+++ mydb.py
@@ -1,10 +1,10 @@
 mydb = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mydb_7iyk_4drd',
-        'USER': 'aliseidu',
-        'PASSWORD': 'bsYEHwYpRjv9eusJy5DQpb4AOBwfxXBU',
-        'HOST': 'dpg-cum84nt6l47c7396bgdg-a.oregon-postgres.render.com',
+        'NAME': 'dcrm_4zyt',
+        'USER': 'dcrm',
+        'PASSWORD': 'nvEPwesUw0Og69c34HNiDLww2YO55XA5',
+        'HOST': 'dpg-cva3dhan91rc738vaim0-a.oregon-postgres.render.com',
         'PORT': '5432',
     }
 }
